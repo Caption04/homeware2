1f3876dc92b0d827923309080490e6281bd2bfb5
Caption04 <kgkatunga04@gmail.com>
2025-10-14 09:51:03 +0200

    greeen

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const featuredSection = document.querySelectorAll(".featured-section");
+window.addEventListener('load', checkTop);
+window.addEventListener('scroll', checkTop);
+
+function checkTop(){
+    featuredSection.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if(rect.top >= 0 && rect.top <= window.innerHeight){
+            section.classList.add("animation");            
+        }
+    })
+}
+
+
+
 const openMenu = document.querySelector(".menu-open-btn");
 const closeMenu = document.querySelector(".close-menu-btn");
 const sideMenu = document.querySelector(".side-menu");
@@ -22,7 +37,14 @@ closeMenu.addEventListener("click", () => {
     document.documentElement.style.overflow = 'auto';
     overlay.classList.remove("open");
     sideMenu.classList.remove("open");
+    setInterval(() => {
+        sideMenu.style.background = "black";
+        sidebarData.forEach(data => {
+            data.classList.remove("side-bar-animation");
+        })
+    }, 1000);
 })
+
 
 
 
